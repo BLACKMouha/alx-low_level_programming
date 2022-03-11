6c42bdf001763924efdfa19f90de773fa78ce40b
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-11 14:51:55 -0800

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+nclude <stdio.h>
+
+/**
+ * main - print single digit numbers
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	
+	for (i = 97; i < (97 + 26); i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
