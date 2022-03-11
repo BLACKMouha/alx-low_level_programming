d7f895fca700399cfdfcd0481ccc459256a4812a
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-11 01:41:43 -0800

    Size of a variable type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,11 @@ int main(void)
 	float f;
 	long int li;
 	long long int lli;
-	
-	printf("Size of char: %lu bytes\n",(unsigned long)sizeof(c));
-	printf("Size of int : %lu bytes\n",(unsigned long)sizeof(i));
-	printf("Size of long int: %lu bytes\n",(unsigned long)sizeof(li));
-	printf("Size of long long: int %lu bytes\n",(unsigned long)sizeof(lli));
-	printf("Size of float: %lu bytes\n",(unsigned long)sizeof(f));
+	printf("Size of char: %lu bytes\n", (unsigned long)sizeof(c));
+	printf("Size of int : %lu bytes\n", (unsigned long)sizeof(i));
+	printf("Size of long int: %lu bytes\n", (unsigned long)sizeof(li));
+	printf("Size of long long int: %lu bytes\n", (unsigned long)sizeof(lli));
+	printf("Size of float: %lu bytes\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
