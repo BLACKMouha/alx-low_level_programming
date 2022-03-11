b86236d85607754d2d3829b23f4c53c839ea478b
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-11 00:05:47 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main : Entry Point
  * Return : Always 0 (success)
  */
-int main (void) {
-	puts("\"Programming is like building a multilingual puzzle");
+int main (void) 
+{
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
