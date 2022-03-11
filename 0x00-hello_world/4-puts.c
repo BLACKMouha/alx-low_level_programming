5f5a79d170a40c65c480bbbf897e4ecf16abe129
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-10 23:53:16 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main : Entry Point
+ * Return : Always 0 (success)
+ */
+int main (void) {
+	puts("\"Programming is like building a multilingual puzzles");
+	return (0);
+}
