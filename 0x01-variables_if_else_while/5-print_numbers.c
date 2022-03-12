b3513614ccdf32978da6b7ab18afbaf6de249e75
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-11 23:58:55 -0800

    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+lude <stdio.h>
+
+/**
+ * main - Ehtry point
+ * Return: Always 0 (success)
+*/
+ 
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar(10);
+
+	return (0);
+}
