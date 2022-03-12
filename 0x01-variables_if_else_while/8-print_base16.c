17df3d431fef8da501fe9ab133ba678f48522c39
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-12 01:41:46 -0800

    Hexadecimal in C

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main- Entry point
+ *Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+	for (c = 'a'; c <= 'a'+ 5; c++)
+	{
+		putchar(c);
+	}
+
+	putchar(10);
+
+	return (0);
+}
