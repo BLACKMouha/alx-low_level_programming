a090e46adbcb4a562cfeabf65b5e403923e7b8df
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-12 03:38:43 -0800

    Combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			for (k = j + 1; k <= 57; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if ((i + j + k) < (55 + 56 + 57))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+
+	}
+
+	putchar(10);
+
+	return (0);
+}
