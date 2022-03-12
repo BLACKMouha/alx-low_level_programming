55804da870cdbdea9186bf7c9a6d9d10e9022bde
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-11 23:50:04 -0800

    Alphabet without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Ehtry point
+ * Return: Always 0 (success)
+*/
+ 
+int main()
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if ((i != 101) && (i != 113))
+		{
+			putchar(i);
+		}
+	}
+
+	return 0;
+}
