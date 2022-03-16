c0e194e01d5ca55a5b36b5f2965b4cb225267293
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-16 16:28:32 -0700

    Protoype of print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,8 +10,18 @@
 
 int print_last_digit(int i)
 {
-	int m;
-	m = i % 10;
-
-	return m;
+	int m; 
+	
+	m= i % 10;
+	if (m < 0)
+	{
+		_putchar(-m + 48);
+		return (-m);
+	}
+	else
+	{
+		_putchar(m + 48);
+		
+		return (m);
+	}
 }
