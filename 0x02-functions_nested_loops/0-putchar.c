87ca3e0242299e7c8b1230ec40582f4b7818c3c3
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-16 04:32:10 -0700

    Update 0-puchat.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,15 +10,15 @@ int _putchar (char c);
 
 int main(void)
 {
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar(10);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar(10);
 
-  return (0);
+	return (0);
 }
