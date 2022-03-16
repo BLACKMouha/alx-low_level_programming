aedb14e9ff3b58a74d88552cfb3d25153d73bd8c
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-16 07:16:22 -0700

    Alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar (char c);
+#endif
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+
+int main (void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar (i);
+	}
+
+	return (0);
+}
