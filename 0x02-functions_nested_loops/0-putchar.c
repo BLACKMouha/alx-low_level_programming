243fe07f926b2a4fa30fab01aa9fe275e0f44d67
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-16 11:25:49 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,19 +6,19 @@ int _putchar (char c);
 /**
  * main - check the code
  * Return : Always 0
- */
+*/
 
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar(10);
-    
-    return (0);
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar(10);
+
+  return (0);
 }
