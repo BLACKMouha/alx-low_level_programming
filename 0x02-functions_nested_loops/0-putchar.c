993e8aa7f81fe8a37defd97250f4289c48dfb614
BLACKMouha <96888801+BLACKMouha@users.noreply.github.com>
2022-03-16 11:11:17 +0000

    Update 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,6 @@
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- * 
- * main - Entry point
+ * main - check the code
  * Return : Always 0 (success)
  */
 
