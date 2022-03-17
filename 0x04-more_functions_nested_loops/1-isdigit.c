4a0ab99eed677c4577aced9a3123028fcf9d4412
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-17 18:29:06 +0000

    Digit or not

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,13 @@
 #include "main.h"
-#include <ctype.h>
 
 /**
- * _isdigit - Checks if a given parameter is a number of not
- * @c: an ASCII character in integer format
- * Return: 1 if number otherwise 0 
-*/
-
+ * _isdigit - Checks if a given parameter is a digit
+ * @c: ASCII character in integer format
+ *
+ * Return: 1 if digit, other case 0.
+ */
 int _isdigit(int c)
 {
-	return (c >=48 && c <= 57) ? 1 : 0;
+        return ((c >= 48 && c <= 57) ?1 :0);
 }
-
 
