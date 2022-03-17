e5db45814689fd3283c809fe0a03e8168bc5a949
BLACKMouha <mohamedsenghor05@gmail.com>
2022-03-16 18:11:22 -0700

    24 hours Chrono

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - This function prints out the time from 00:00 t0 23:59
+ *
+ * Return: Void
+*/
+
+void  jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i<= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for(l = 48; l <= 57; l++)
+				{
+					if(i >= 50 && b>= 52)
+					{
+						break;
+					}
+					_putchar(a);
+					_putchar(b);
+					_puchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar(10);
+				}
+			}
+		}
+	}
+}
