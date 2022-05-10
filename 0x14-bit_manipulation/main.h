d384481119ced52395682fc6da18c96ed305a05c
BLACKMouha <mohamedsenghor05@gmail.com>
2022-05-10 16:44:30 +0000

    Create main header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * AUthor: Ukonu Divine Chisom
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-bit_manipulation directory.
+ */
+int _pow(int base, int power);
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+#endif /* MAIN_H */
