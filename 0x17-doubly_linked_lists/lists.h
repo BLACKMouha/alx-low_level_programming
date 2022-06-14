a84652d20608a745a53733e9a6a525c32fa68df7
BLACKMouha <mohamedsenghor05gmail.com>
2022-06-14 12:18:49 +0000

    Header files

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: data stored in integer format
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description:  douly linked list
+ */
+typedeef struct dlistint_s
+{
+  int n;
+  struct dlistint_s *prev;
+  struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+#endif /* _LIST_H_"
