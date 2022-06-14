be173a883d270f4b5cba6c1ce82572881e5cb35c
BLACKMouha <mohamedsenghor05gmail.com>
2022-06-14 12:29:51 +0000

    Printing all the elements of a doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,16 +13,16 @@ size_t print_dlistint(const dlistint_t *h)
   unsigned int number_of_nodes = 0; /* The returned number of nodes*/
   dlistint_t *current_node; /* this variable will traverse all the DLL*/
 
-    if (h)
+  if (h)
+  {
+    for (current_node = h; current_node; current_node = current_node->next)
     {
-      for (current_node = h; current_node; current_node = current_node->next)
-      {
-        printf("%d\n", current_node->n);
-        number_of_nodes++;
-      }
-
-      return (number_of_nodes);
+      printf("%d\n", current_node->n);
+      number_of_nodes++;
     }
 
-    return (0);
+    return (number_of_nodes);
+  }
+
+  return (0);
 }
