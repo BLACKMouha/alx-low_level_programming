cde674d718c7568b61c704f207419d9fb20a4110
BLACKMouha <mohamedsenghor05gmail.com>
2022-06-14 16:41:12 +0000

    Function returning the length of a doubly linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,16 +1,13 @@
 #include "lists.h"
-
 /**
- * dlistint_len - function that returns the number of nodes of a dlistint_t list
- * Prototype: size_t dlistint_len(const dlistint_t *h);
- * @h: the head of the dlistint_t list
- * Return: the number of nodes in a linked dlistint_t list
+ * dlistint_len - length of a doubly linked list
+ * @h: list's head
+ * Return: number of nodes
  */
-
 size_t dlistint_len(const dlistint_t *h)
 {
-	const dlistint_t *current_node; /* list runner */
-	int number_of_nodes = 0; /* lenght counter */
+	const dlistint_t *current_node; /* traverse all the list */
+	int number_of_nodes = 0; /* number of nodes */
 
 	for (current_node = h; current_node; current_node = current_node->next)
 		number_of_nodes++;
