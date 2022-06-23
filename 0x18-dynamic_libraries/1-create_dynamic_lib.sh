1e0194b116485d1df7b0108b6e7e9547c3efff54
BLACKMouha <mohamedsenghor05gmail.com>
2022-06-23 21:30:21 +0000

    Script that creates a dynamic library called liball.so from all the .c files that are in the current directory

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -2,5 +2,5 @@
 #Create a script that creates a dynamic library called liball.so
 #from all .c files that in the current directory
 
-gcc *.c -c -fPIC
+gcc -Wall -Werror -c -fPIC *.c
 gcc *.o - shared -o liball.so
