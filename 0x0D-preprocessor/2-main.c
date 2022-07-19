a7a52b3f8be37ebab051f1e4e40da38e9d8a6dbc
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-19 19:08:48 +0000

    Upload files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the current file name
+ *
+ * Returns: Always EXIT_SUCCESS
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
+}
