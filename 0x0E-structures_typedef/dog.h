3f28b2973534458cca1e323fceb2b2d014919cba
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-19 19:11:39 +0000

    Upload files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef DOH_H
+#define DOG_H
+
+/**
+ * dog - structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog;
+ *
+ * Description: informations about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/**
+ * dog_t - typdef for struct dog
+ */
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif /** DOG_H */
