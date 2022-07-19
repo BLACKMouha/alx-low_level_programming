30558ff706b5adb59973916ba2e4503999d695ca
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-19 19:03:21 +0000

    Upload files

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,131 +1,151 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _abs - computes the absolute value of a given integer
- * Prototype: unsigned int _abs(int n);
- * @n: integer
- * Return an integer representing its absolute value
- */
-unsigned int _abs(int n)
+ * _atoi_digit - convert a char to integer.
+ * @x: character to convert.
+ * Return: integer.
+ **/
+
+int _atoi_digit(char x)
 {
-	if (n < 0)
-		return (-n);
-	else
-		return (n);
+	unsigned int res;
+
+	if (x <= '9' && x >= '0')
+		res = x - '0';
+	return (res);
 }
-
-
 
 /**
- * main - multiplies two positive numbers
- * Prototye: int main(int ac, char **av);
- * @ac: length of @av
- * @av: array of strings
- * Return: EXIT_SUCCESS in success
- * Usage: mul num1 num2
- */
-
-int _isdigit(char c)
+ * _isNumber - Define if a string is a number.
+ * @argv: Pointer to string.
+ * Return: success (0).
+ **/
+int _isNumber(char *argv)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (-1);
+	int i;
+	for (i = 0; argv[i]; i++)
+		if (argv[i] < 48 || argv[i] > 57)
+			return (1);
+	return (0);
 }
 
-int main(int ac, char **av)
+/**
+ *_calloc - allocate array of size * nmemb.
+ * @nmemb: number of elements.
+ * @size: size of element.
+ * Return: pointer to array.
+ **/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned long int num1, num2;
-	int i = 0; j = 0;
-	if (ac < 3 || ac > 3)
+	char *tab;
+	unsigned int i;
+
+	tab = malloc(size * nmemb);
+
+	if (tab == NULL)
+		return (NULL);
+
+	for (i = 0; i < (size * nmemb); i++)
+		tab[i] = '0';
+
+	return (tab);
+
+}
+
+/**
+ * mul_array - multiply two arrays.
+ * @a1: first array.
+ * @len1: length of array a1.
+ * @a2:  char.
+ * @a3: array for result.
+ * @lena: length of array a3.
+ * Return: pointer to array.
+ **/
+
+void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
+{
+	int mul = 0, i, k;
+
+	k = lena;
+	for (i = len1 - 1; i >= 0 ; i--)
 	{
-		printf("Usage: mul num1 num2\n");
+		mul += (a1[i] - '0') * (a2 - '0') + (a3[k] - '0');
+		a3[k] = (mul % 10) + '0';
+		mul /= 10;
+		k--;
+	}
+	while (mul != 0)
+	{
+		mul += a3[k] - '0';
+		a3[k] = (mul % 10) + '0';
+		mul /= 10;
+		k--;
+	}
+
+	return (a3)
+}
+
+/**
+ * print_array - print all digits of array.
+ * @nb: number of elements to print.
+ * @a: array of elements.
+ **/
+void print_array(char *a, int nb)
+{
+	int i = 0;
+
+	while (a[i] == '0' && (i + 1) < nb)
+	{
+		i++;
+	}
+	for (; i < nb; i++)
+	{
+		_putchar(a[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+ *main - print the multiplication of 2 numbers.
+ *@argc: array length.
+ *@argv: array.
+ *Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, c, len1, len2, lenres;
+	char E[6] = {'E', 'r', 'r', 'o', 'r', '\n'};
+	char *tabres;
+
+	if (argc != 3 || _isNumber(argv[1]) == 1 || _isNumber(argv[2]) == 1)
+	{
+		for (i = 0; i < 6; i++)
+		{
+			_putchar(E[i]);
+		}
 		exit(98);
 	}
-
-	while (av[1][i] != '\0')
+	for (len1 = 0; argv[1][len1]; len1++)
+		;
+	for (len2 = 0; argv[2][len2]; len2++)
+		;
+	lenres = len1 + len2;
+	tabres = _calloc(lenres, sizeof(int));
+	if (tabre == NULL)
 	{
-		if (_isdigit(av[1][i] == -1)
-		{
-			printf("Error\n");
-			exit(98);
-		}
-		i++
+		free(tabres);
+		return (0);
 	}
-	i = 0;
-	while (av[2][i] != '\0')
+	for (i = len2 - 1, c = 0; i >= 0; i--)
 	{
-		if (_isdigit(av[2][i] == -1)
-		{
-			printf("Error\n");
-			exit(98);
-		}
-		i++
+		tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 - c));
+		c++;
 	}
-
-	num1 = atoi(_abs(av[1]));
-	num2 = atoi(_abs(av[2]));
-
-
+	print_array(tabres, lenres);
+	free(tabres);
+	exit(EXIT_SUCCESS);
+	return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
