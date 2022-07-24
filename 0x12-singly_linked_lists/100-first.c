a9378126a1c291751b693983e269a427f0b5401b
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-24 21:20:36 +0000

    Function executed before the main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,10 +5,10 @@ void before_main(void) __attribute__((constructor));
  * before_main - function executed before and prints a text
  * Prototype: void before_main(void) __attribute__((constructor));
  * Explanations:
- *	When we want a function to be executed before the main function, we
- * 	have to precise the "constructor" attribute.
- *	Note: It's possible to see a function executed after the main. In
- *	this case we use the "destructor" attribute.
+ * When we want a function to be executed before the main function, we
+ * have to precise the "constructor" attribute.
+ * Note: It's possible to see a function executed after the main. In
+ * this case we use the "destructor" attribute.
  *
  * Return: Nothing
  */
