4cd77d27718ac2bd95f878a07cd6d4028b3a50b4
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-24 20:16:10 +0000

    Function that frees an entire list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - free a list_t list
+ * Prototype: void free_list(list_t *head);
+ * @head: points to the first element of a list_t list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(temp->str);
+		free(temp);
+		head = temp;
+	}
+}
