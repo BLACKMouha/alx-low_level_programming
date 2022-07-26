533381b3179e8713ed2ea98a9675ef065e5bbee5
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-26 17:46:59 +0000

    Function that founds a node at a given index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,30 @@
 #include "lists.h"
 
 /**
+ * listint_len - computes the number of node of a listint_t list
+ * Prototype: size_t listint_len(const listint_t *h);
+ * @h: points to the first node
+ * Return: the number of nodes of a listint_t list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t non;
+
+	non = 0;
+	if (h)
+	{
+		while (h != NULL)
+		{
+			h = h->next;
+			non++;
+		}
+		return (non);
+	}
+	else
+		return (0);
+}
+
+/**
  * get_nodeint_at_index - finds the nth node of a listint_t list
  * Prototype:
  * listint_t *get_nodeint_at_index(listint_t *head; unsigned int
