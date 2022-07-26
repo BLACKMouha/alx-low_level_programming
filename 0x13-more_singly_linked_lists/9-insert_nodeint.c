3efa6821670498dfba9087f52bde5e0387616cf8
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-26 19:15:52 +0000

    Function that insert a new node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,151 +1,109 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given index
- * Prototype:
- * insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
- * @head: points to the first node
- * @idx: index of the list where the new node should be added. Index
- * starts to 0
- * @n: data to be inserted into the new node
- * Return: the address of the new node, or NULL on failure. If it's
- * not possible to add the new node at index @idx, it does not add
- * the new node and it returns NULL
+ * listint_len - counts all the elements of a struct list type listint_t
+ *
+ * @h: pointer to the head/first member of a singly linked list of structs
+ * of type listint_s
+ *
+ * Return: amount of nodes in the list
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
-	listint_t *node_at_index;
-	listint_t *new_node;
-	size_t non;
 
-	if (idx > non)
-		return (NULL);
-
-	if (!head)
-		return (NULL);
-
-	node_at_index = get_nodeint_at_index(*head, (idx - 1));
-	non = listint_len(*head);
-
-
-	if (idx == 0)
-		add_nodeint(head, n);
-	else if (idx == non)
-		add_nodeint_end(head, n);
-
-	new_node = malloc(sizeof(new_node));
-	if (new_node == NULL)
-		return (NULL);
-
-	if (!head)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	for (i = 0; (i < (idx - 1)); i++)
-		temp = temp->next;
-
-	new_node->n = n;
-	new_node->next = node_at_index->next;
-	node_at_index->next = new_node;
-	
-	return (new_node);
-}
-
-
-
-/** ==============START===LISTINT_LEN===START===================== */
-/**
- * listint_len - computes the number of node of a listint_t list
- * Prototype: size_t listint_len(const listint_t *h);
- * @h: points to the first node
- * Return: the number of nodes of a listint_t list
- */
 size_t listint_len(const listint_t *h)
 {
-	size_t non;
+	size_t count = 1;
 
-	non = 0;
 	if (h)
 	{
-		while (h != NULL)
-		{
-			h = h->next;
-			non++;
-		}
-		return (non);
+		if (h->next)
+			count += listint_len(h->next);
+
+		return (count);
 	}
 	else
 		return (0);
 }
-/** ==============END===LISTINT_LEN===END======================= */
 
-/** ==============START===ADD_NODEINT===START=================== */
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list
- * Prototype: listint_t *add_nodeint(listint_t **head, const int n);
- * @head: points to the first node
- * @n: integer to be inserted in the n field of a listint_t node
- * Return: the address of the newly node created or NULL on failure
+ * add_nodeint - adds a new node to the start of a struct list type listint_t
+ *
+ * @head: double pointer to the first member of a singly linked list
+ * of structs of type listint_s
+ *
+ * @n: int to store in n member of new listint_s struct
+ *
+ * Return: address of the new element, or NULL if it failed
  */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
+	listint_t *new_n;
 
-	if (!n)
+	new_n = malloc(sizeof(listint_t));
+	if (!new_n)
+	{
+		free(new_n);
 		return (NULL);
+	}
 
-	new = malloc(sizeof(new));
-	if (new == NULL)
-		return (NULL);
+	new_n->n = n;
+	new_n->next = *head;
+	*head = new_n;
 
-	new->n = n;
-
-	if (*head == NULL)
-		new->next = NULL;
-	else
-		new->next = *head;
-
-	*head = new;
-
-	return (new);
+	return (new_n);
 }
-/** ===============END===ADD_NODEINT===END===================== */
 
-/** ==============START===ADD_NODEINT===START================== */
 /**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * Prototype: listint_t *add_nodeint_end(listint_t **head,
- * const int n)
- * @head: points to the first element
- * @n: integer to be hold by the n field of a listint_t node
+ * insert_nodeint_at_index - inserts a new node into a listint_t linked list
+ * at index position idx with n value
  *
- * Return: the address of the new element or NULL on failure
+ * @head: double pointer to the first member of a singly linked list
+ * of structs of type listint_s
+ *
+ * @idx: index value of position in list, starting at 0
+ *
+ * @n: int value to be stored in new node at index idx
+ *
+ * Return: address of new node, or NULL if failed
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *last_node;
+/* can it be done with less helpers? try refactoring later */
+	size_t list_len;
+	listint_t *new_n;
+	listint_t *temp = *head;
+	unsigned int i;
 
-	last_node = *head;
+	if (!head)
+		return (NULL);
 
-	new_node = malloc(sizeof(new_node));
-	if (!new_node)
+	list_len = listint_len(*head);
+
+	if (idx > list_len)
+		return (NULL);
+
+	if (idx == list_len)
+		return (add_nodeint_end(head, n));
+
+	if (idx == 0)
+		return (add_nodeint(head, n));
+
+	new_n = malloc(sizeof(listint_t));
+	if (!new_n)
+		return (NULL);
+
+	if (!*head)
 	{
-		free(new_node);
+		free(new_n);
 		return (NULL);
 	}
-	new_node->n = n;
-	new_node->next = NULL;
-
-	if (!(*head))
-		*head = new_node;
-	else
+	for (i = 0; (i < (idx - 1)); i++)
 	{
-		while (last_node->next != NULL)
-			last_node = last_node->next;
-		last_node->next = new_node;
+		temp = temp->next;
 	}
-
-	return (new_node);
+	new_n->n = n;
+	new_n->next = temp->next;
+	temp->next = new_n;
+	return (new_n);
 }
-/** ==============END===ADD_NODEINT_END===END=================== */
