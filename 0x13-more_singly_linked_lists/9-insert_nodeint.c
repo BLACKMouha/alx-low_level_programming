2fd5c6598478cfcda248d7745ce90592ba3ac8a7
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-26 18:41:09 +0000

    Function that insert a new node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -92,6 +92,36 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 }
 
 /**
+ * get_nodeint_at_index - finds the nth node of a listint_t list
+ * Prototype:
+ * listint_t *get_nodeint_at_index(listint_t *head; unsigned int
+ * index)
+ * @head: points to the first node
+ * @index: the index of the node to be found, starting to 0
+ * Return: the address of the node to be found. If the node does not
+ * extst, NULL is returned
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node_at_index;
+	unsigned int runner;
+	size_t non = listint_len(head);
+
+	if (index > non)
+		return (NULL);
+
+	runner = 0;
+	while (head && runner < index)
+	{
+		head = head->next;
+		runner++;
+	}
+	node_at_index = head;
+
+	return (node_at_index);
+}
+
+/**
  * insert_nodeint_at_index - inserts a new node at a given index
  * Prototype:
  * insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
