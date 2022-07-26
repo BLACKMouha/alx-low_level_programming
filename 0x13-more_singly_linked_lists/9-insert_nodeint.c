efecfbf14b0f3fcc567c7fc1306ab3b1bbabe41b
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-26 18:27:55 +0000

    Function that insert a new node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,97 @@
 #include "lists.h"
 
 /**
+ * listint_len - computes the number of node of a listint_t list
+ * Prototype: size_t listint_len(const listint_t *h);
+ * @h: points to the first node
+ * Return: the number of nodes of a listint_t list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t non;
+
+	non = 0;
+	if (h)
+	{
+		while (h != NULL)
+		{
+			h = h->next;
+			non++;
+		}
+		return (non);
+	}
+	else
+		return (0);
+}
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * Prototype: listint_t *add_nodeint(listint_t **head, const int n);
+ * @head: points to the first node
+ * @n: integer to be inserted in the n field of a listint_t node
+ * Return: the address of the newly node created or NULL on failure
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	if (!n)
+		return (NULL);
+
+	new = malloc(sizeof(new));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (*head == NULL)
+		new->next = NULL;
+	else
+		new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * Prototype: listint_t *add_nodeint_end(listint_t **head,
+ * const int n)
+ * @head: points to the first element
+ * @n: integer to be hold by the n field of a listint_t node
+ *
+ * Return: the address of the new element or NULL on failure
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node, *last_node;
+
+	last_node = *head;
+
+	new_node = malloc(sizeof(new_node));
+	if (!new_node)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (!(*head))
+		*head = new_node;
+	else
+	{
+		while (last_node->next != NULL)
+			last_node = last_node->next;
+		last_node->next = new_node;
+	}
+
+	return (new_node);
+}
+
+/**
  * insert_nodeint_at_index - inserts a new node at a given index
  * Prototype:
  * insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
