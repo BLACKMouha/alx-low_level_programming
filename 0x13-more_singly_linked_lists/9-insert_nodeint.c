41115ac40e5ab01a15b6a82400ceed9544ffc67c
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-26 19:15:13 +0000

    Function that insert a new node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -42,11 +42,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		free(new_node);
 		return (NULL);
 	}
+	for (i = 0; (i < (idx - 1)); i++)
+		temp = temp->next;
 
 	new_node->n = n;
 	new_node->next = node_at_index->next;
 	node_at_index->next = new_node;
-	}
+	
 	return (new_node);
 }
 
@@ -147,35 +149,3 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	return (new_node);
 }
 /** ==============END===ADD_NODEINT_END===END=================== */
-
-/** ==========START===GET_NODEINT_AT_INDEX===START============== */
-/**
- * get_nodeint_at_index - finds the nth node of a listint_t list
- * Prototype:
- * listint_t *get_nodeint_at_index(listint_t *head; unsigned int
- * index)
- * @head: points to the first node
- * @index: the index of the node to be found, starting to 0
- * Return: the address of the node to be found. If the node does not
- * extst, NULL is returned
- */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-{
-	listint_t *node_at_index;
-	unsigned int runner;
-	size_t non = listint_len(head);
-
-	if (index > non)
-		return (NULL);
-
-	runner = 0;
-	while (head && runner < index)
-	{
-		head = head->next;
-		runner++;
-	}
-	node_at_index = head;
-
-	return (node_at_index);
-}
-/** ==========END===GET_NODEINT_AT_INDEX===END============== */
