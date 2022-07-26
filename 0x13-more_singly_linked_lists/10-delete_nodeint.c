3dcb7264f6144eeef03e5f727bf50dc51a0c1891
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-26 22:57:13 +0000

    Function that deletes a node at a given index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -68,7 +68,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-/* can it be done with less helpers? try refactoring later */
 	size_t length;
 	listint_t *temp;
 
