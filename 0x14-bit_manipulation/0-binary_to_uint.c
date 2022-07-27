d5f87bfb76eb9c513e774d488a850752b3b05c8f
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-27 13:47:14 +0000

    Function that converts a binary number to an unsingned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,93 +1,37 @@
 #include "main.h"
 
-#include <stddef.h>
-
 /**
- * _pow_recursion - returns the x to the power y
- * @x: the base
- * @y: the exponent
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
  *
- * Return: the integer result, -1 on error
- */
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y > 0)
-		return (x * _pow_recursion(x, y - 1));
-	return (1);
-}
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * Prototype: unsigned int binary_to_uint(const char *b);
- * @b: point to a string of 0 and 1 chars
- * Return: the converted number, or 0 if :
- * - there is one or more chars in the string @b that is not 0 or 1
- * - @b == NULL
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	size_t len_b, i;
-	unsigned int result = 0;
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
 
-	len_b = strlen(b);
-	i = 0;
-	while (b[i] != '\0')
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[i] == '0' || b[i] == '1')
+		if (b[len] != '0' && b[len] != '1')
 		{
-			if (b[i] == '0')
-				result += 0;
-			if (b[i] == '1')
-				result += _pow_recursion(2, (int)len_b);
-		}
-		else
 			return (0);
-		len_b--;
-		i++;
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (result / 2);
+
+	return (ui);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
