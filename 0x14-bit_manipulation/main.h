5fdc333c04807008663a0cfc0aeb6258ddae8196
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-27 13:37:42 +0000

    Header file for this project

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#ifndef MAIN_H
+#define MAIN_H
+
+unsigned int binary_to_uint(const char *b);
+
+
+#endif /** MAIN_H */
