f078f272288fa663c097cf9cea0d83b49ed4458d
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-28 18:29:41 +0000

    Function that converts a binary number to an unsigned integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,41 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - return the length of a string.
- * @str: a const pointer to the string.
- *
- * Return: The string length.
- */
-unsigned int _strlen(char const *str)
-{
-	unsigned int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	return (i);
-}
-
-/**
- * _pow - return the power of a number.
- * @a: the number to be raised.
- * @b: the exponent
- *
- * Return: The power of a number.
- */
-unsigned int _pow(unsigned int a, unsigned int b)
-{
-	unsigned int i, pow = 1;
-
-	if (b == 0)
-		return (1);
-
-	for (i = 0; i < b; i++)
-		pow *= a;
-
-	return (pow);
-}
-
-/**
  * binary_to_uint - Converts a binary number to an unsigned int.
  * @b: A pointer to a string of 0 and 1 chars.
  *
