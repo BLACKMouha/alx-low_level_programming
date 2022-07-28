70fd6f45c6224d573f14521926d3fca9c13dfe94
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-28 19:24:35 +0000

    Function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,31 +10,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int power, i, j, number_of_powers, nop;
-	int *arr_bin;
-	if (n == 0)
-		return (0);
+	if (index > 63)
+		return (-1);
 
-	power = 1;
-	number_of_powers = 0;
-	while (power <= n)
-	{
-		number_of_powers++;
-		power <<= 1;
-	}
-
-	power >>= 1;
-	arr_bin = (int *)malloc(sizeof(int) * number_of_powers);
-	nop = number_of_powers - 1;
-	for (i = power, j = nop; i > 0 && j > 0; i >>= 1, j--)
-	{
-		if ((n & i) == i)
-			*(arr_bin + j) = 1;
-		else
-			*(arr_bin + j) = 0;
-	}
-
-	return (arr_bin[index]);
+	return ((n >> index) & 1);
 }
 
 
