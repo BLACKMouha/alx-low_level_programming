0a96f59c6177427c632ea22a81569850b32660f0
BLACKMouha <mohamedsenghor05gmail.com>
2022-07-31 14:59:07 +0000

    Function that reads a file and prints its content to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,23 +18,123 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t read_chars, written;
-	char *buffer = malloc(sizeof(char) * letters);
+	int fd, sz, oz;
+	char *buf = malloc(sizeof(char) * letters);
 
 	fd = open(filename, O_RDONLY);
-	if ((filename == NULL) || (fd == -1) || (buffer == NULL))
+	if ((filename == NULL) || (fd == -1) || (buf == NULL))
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
-	read_chars = read(fd, buffer, letters);
-	if (read_chars == -1)
+	sz = read(fd, buf, letters);
+	if (sz == -1)
 		return (0);
-	buffer[read_chars] = '\0';
-	written = write(STDOUT_FILENO, buffer, read_chars);
-	if (written == -1)
+	buf[sz] = '\0';
+	oz = write(STDOUT_FILENO, buf, sz);
+	if (oz == -1)
 		return (0);
 	close(fd);
-	return (written);
+	return (oz);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
