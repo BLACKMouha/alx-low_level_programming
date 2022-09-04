dc28f52a21cca0b54486ba46f0b9d8154146ff5d
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-04 03:12:23 +0000

    Function that adds a new node inside a dlistint_t list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,21 +13,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	if (!head)
-		return (NULL);
-
-	new = malloc(sizeof(dlistint_t));
+	new = malloc(sizeof(*new));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
+
 	if (*head != NULL)
 		(*head)->prev = new;
-	(*head) = new;
-	return (new);
 
+	*head = new;
+
+	return (new);
 }
 
 
