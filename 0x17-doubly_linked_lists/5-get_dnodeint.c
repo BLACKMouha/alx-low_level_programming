df53eeb13e9f9723ebbd4183f366322e2c5a1991
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-04 03:59:59 +0000

    Function that returns the node at a given index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,32 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - computes the number of nodes of a dlistint_t list
+ *
+ * Prototype: size_t dlistint_t(dlistint_t *h);
+ * @h: points to the first node
+ *
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	if (!h)
+		return (0);
+
+	i = 0;
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
+
+
+/**
  * get_dnodeint_at_index - finds the nth node of a dlistint_t list
  * Prototype:
  * dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
