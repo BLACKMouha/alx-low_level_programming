9dd2195677128a54d9dc1112bcd7366ad6218c99
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-19 22:39:31 +0000

    Printing to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - this is the main function
+*
+* Return: 0 if the function runs without an error
+*/
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(str, 50, 1, stderr);
+	return (1);
+}
