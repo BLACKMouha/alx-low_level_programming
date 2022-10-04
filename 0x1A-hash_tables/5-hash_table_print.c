8b65c4d221ce3b50fe0a098280303b8fed5bdec4
BLACKMouha <mohamedsenghor05gmail.com>
2022-10-04 11:31:41 +0000

    function that print a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,73 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * Prototype: void hash_table_print(const hash_table_t *ht);
+ * @ht: a hash table
+ * Return: Nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	unsigned int idx;
+	char comma = 0;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		if (ht->array[idx])
+		{
+			tmp = ht->array[idx];
+			while (tmp)
+			{
+				if (comma == 1)
+					printf(", ");
+				printf("'%s':'%s'", tmp->key, tmp->value);
+				tmp = tmp->next;
+				comma = 1;
+			}
+		}
+	}
+	printf("}\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
