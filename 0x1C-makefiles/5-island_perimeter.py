36ba00a769ce5a60ee5fa17919516d661a4d976b
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-13 18:55:07 +0000

    Computes the perimeter of an island represented by '1' and 0 where '1' is the island and 0 is the water

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+5-island_perimeter module
+Computes the perimeter of the island described in grid
+'''
+
+
+def island_perimeter(grid):
+    n, sides = 0, 4
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                n += sides
+                if (grid[i - 1][j] == 1 and i > 0):
+                    n -= 1
+                if (grid[i + 1][j] == 1):
+                    n -= 1
+                if (grid[i][j - 1] == 1 and j > 0):
+                    n -= 1
+                if (grid[i][j + 1] == 1 and j > 0):
+                    n -= 1
+    return (n)
